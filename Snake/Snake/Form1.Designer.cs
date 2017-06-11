0e595c9eebf400de4c289e2dbfa34f02fcf1e192
Filipens1994 <filipens1994@gmail.com>
2017-06-11 18:20:51 +0100

    Postęp 1

---
+++ Snake/Snake/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblMenu.Location = new System.Drawing.Point(0, 0);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(308, 212);
+            this.lblMenu.TabIndex = 0;
+            this.lblMenu.Text = "Snake\r\n\r\nKorzystaj ze strzałek, aby zmieniać kierunek\r\nruchu węża\r\n\r\nUżyj spacji " +
+    "do zaytrzymania gry\r\n\r\nNaciśnij Enter by rozpocząć grę\r\n";
+            this.lblMenu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 212);
+            this.Controls.Add(this.lblMenu);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(316, 239);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(316, 239);
+            this.Name = "Form1";
+            this.Text = "Snake - Score: 1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMenu;
+    }
+}
+
